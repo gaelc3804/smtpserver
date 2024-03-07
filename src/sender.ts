7c7d8de07be77aa5d3e8e7117d01b1a3a5c6ecc4
Your Name <you@example.com>
2024-03-07 04:25:44 -0300

    update params

--- src/sender.ts
+++ src/sender.ts
@@ -1,14 +1,6 @@
 import nodemailer from 'nodemailer'
 
-const transporter = nodemailer.createTransport({
-  // host: "",
-  port: 465,
-  secure: true, // Use `true` for port 465, `false` for all other ports
-  // auth: {
-  //   user: "maddison53@ethereal.email",
-  //   pass: "jn7jnAPss4f63QBp6D",
-  // },
-});
+const transporter = nodemailer.createTransport({ port: 25, tls: { rejectUnauthorized: false }, ignoreTLS: true });
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
