9f9e949289dd102491c62a0090f279c1775f525c
Your Name <you@example.com>
2024-03-07 04:17:48 -0300

    mailer

--- src/sender.ts
+++ src/sender.ts
@@ -1,5 +1,29 @@
-import SMTPConnection from "nodemailer/lib/smtp-connection"
+import nodemailer from 'nodemailer'
 
-let connection = new SMTPConnection({ port: 3456, host: '127.0.0.1' })
+const transporter = nodemailer.createTransport({
+  // host: "",
+  port: 25,
+  secure: true, // Use `true` for port 465, `false` for all other ports
+  // auth: {
+  //   user: "maddison53@ethereal.email",
+  //   pass: "jn7jnAPss4f63QBp6D",
+  // },
+});
 
-// connection.send({ from: '' }, "Test-02", () => {})
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Maddison Foo Koch 👻" <teste12@forneriahotellery.com.br>', // sender address
+    to: "gaelc3804@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Ola, seja bem vindo</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
+
